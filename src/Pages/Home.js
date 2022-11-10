43962487ef393c6bb98e7250846cb153d2a9da29
bar-85 <bajwolskabarbara@gmail.com>
2022-11-11 00:25:59 +0100

    Add json, tailwind, axios, Home.js and Navbar.js

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full h-full flex-col px-10 py-8'>
+      <h1>Strona Główna</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-16 bg-blue-600'>
+
+    <h1 className='text-white text-3xl font-semibold font-Montserrat'>CRUD APP</h1>
+    </div>
+  )
+}
+
+export default Navbar
