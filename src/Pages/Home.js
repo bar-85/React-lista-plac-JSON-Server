52b433c092eb89c2393e5594e2bdcc058c427155
bar-85 <bajwolskabarbara@gmail.com>
2022-11-11 23:16:08 +0100

    add table

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Home = () => {
 	return (
 		<div className='w-full h-full justify-center items-center flex flex-col px-10 py-8'>
-			<h1 className='text-purple-700 text-3xl font-semibold font-Montserrat'>Strona Główna</h1>
+			<h1 className='text-purple-700 text-3xl font-semibold font-Montserrat'>Lista pracowników</h1>
 			<div class='flex flex-col'>
 				<div class='overflow-x-auto sm:-mx-6 lg:-mx-8'>
 					<div class='py-4 inline-block min-w-full sm:px-6 lg:px-8'>
 						<div class='overflow-hidden'>
-							<table class='min-w-full text-center mt-3'>
+							<table class='min-w-full text-center'>
 								<thead class='border-b bg-purple-600'>
 									<tr>
 										<th scope='col' class='text-sm font-medium text-white px-6 py-4'>
@@ -26,6 +26,9 @@ const Home = () => {
 										<th scope='col' class='text-sm font-medium text-white px-6 py-4'>
 											Do wypłaty (zł)
 										</th>
+                    <th scope='col' class='text-sm font-medium text-white px-6 py-4'>
+											Akcja
+										</th>
 									</tr>
 								</thead>
 								<tbody>
@@ -35,6 +38,11 @@ const Home = () => {
 										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>Kowalski</td>
 										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>mkowalski@gmail.com</td>
 										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>1000</td>
+										<td class='flex justify-center items-center space-x-4 mt-4'>
+                      <button>Pokaż</button>
+                      <button>Edytuj</button>
+                      <button>Usuń</button>
+                      </td>
 									</tr>
 								</tbody>
 							</table>
