d960423cd3adbcc0ed53013ae8b7a89b9ba666e4
bar-85 <bajwolskabarbara@gmail.com>
2022-11-11 22:52:40 +0100

    add button in Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,49 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div className='w-full h-full flex-col px-10 py-8'>
-      <h1>Strona Główna</h1>
-    </div>
-  )
+	return (
+		<div className='w-full h-full justify-center items-center flex flex-col px-10 py-8'>
+			<h1 className='text-purple-700 text-3xl font-semibold font-Montserrat'>Strona Główna</h1>
+			<div class='flex flex-col'>
+				<div class='overflow-x-auto sm:-mx-6 lg:-mx-8'>
+					<div class='py-4 inline-block min-w-full sm:px-6 lg:px-8'>
+						<div class='overflow-hidden'>
+							<table class='min-w-full text-center mt-3'>
+								<thead class='border-b bg-purple-600'>
+									<tr>
+										<th scope='col' class='text-sm font-medium text-white px-6 py-4'>
+											Id
+										</th>
+										<th scope='col' class='text-sm font-medium text-white px-6 py-4'>
+											Imię
+										</th>
+										<th scope='col' class='text-sm font-medium text-white px-6 py-4'>
+											Nazwisko
+										</th>
+                    <th scope='col' class='text-sm font-medium text-white px-6 py-4'>
+											Email
+										</th>
+										<th scope='col' class='text-sm font-medium text-white px-6 py-4'>
+											Do wypłaty (zł)
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class='bg-white border-b'>
+										<td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>1</td>
+										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>Marek</td>
+										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>Kowalski</td>
+										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>mkowalski@gmail.com</td>
+										<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>1000</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Home
--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='w-full h-16 bg-blue-600'>
-
-    <h1 className='text-white text-3xl font-semibold font-Montserrat'>CRUD APP</h1>
-    </div>
-  )
+	return (
+		<div className='w-full h-16 bg-purple-600 flex items-center px-10 py-2 justify-between'>
+			<h1 className='text-white text-3xl font-semibold font-Montserrat'>Lista płac</h1>
+			<button className='bg-transparent hover:bg-purple-500 font-semibold text-white py-2 px-4 border hover:border-white rounded'>Dodaj</button>
+		</div>
+	)
 }
 
 export default Navbar
