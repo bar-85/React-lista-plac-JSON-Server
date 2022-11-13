7496b8618325211301ceccd4dba1d77845dd33e6
bar-85 <bajwolskabarbara@gmail.com>
2022-11-13 21:54:07 +0100

    add addUsers.js

--- src/Pages/AddUser.js
+++ src/Pages/AddUser.js
@@ -1,14 +1,68 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddUser = () => {
-  return (
-    <div className='w-screen h-full justify-center flex flex-col items-center mt-16'>
-      <h1 className='text-black text-2xl font-semibold font-Montserrat m-4 uppercase'>Dodaj pracownika</h1>
-      <form className='w-[80%] h-full flex flex-col justify-center items-center'>
-        <input type="text" placeholder='Imię i nazwisko' className='w-[80%] bg-white/10 text-xl font-Montserrat outline-none py-4 pl-6 border border-violet-600' />
-      </form>
-    </div>
-  )
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [phone, setPhone] = useState('')
+	const [paycheck, setPaycheck] = useState('')
+
+	const navigate = useNavigate()
+
+	const data = {
+		name: name,
+		email: email,
+		phone: phone,
+		paycheck: paycheck,
+	}
+
+	const Submit = e => {
+		e.preventDefault()
+
+		axios.post('http://localhost:3001/users', data).then(navigate('/'))
+	}
+
+	return (
+		<div className='w-screen h-full justify-center flex flex-col items-center mt-16'>
+			<h1 className='text-black text-2xl font-semibold font-Montserrat m-4 uppercase'>Dodaj pracownika</h1>
+			<form className='w-[80%] h-full flex flex-col justify-center items-center'>
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					type='text'
+					placeholder='Imię i nazwisko'
+					className='px-3 py-1.5 text-base font-normal text-gray-700 bg-purple-50 bg-clip-padding rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:outline-none border border-purple-600'
+				/>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type='text'
+					placeholder='Email'
+					className='px-3 py-1.5 text-base font-normal mt-4 text-gray-700 bg-purple-50 bg-clip-padding rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:outline-none border border-purple-600'
+				/>
+				<input
+					value={phone}
+					onChange={(e) => setPhone(e.target.value)}
+					type='phone'
+					placeholder='Telefon'
+					className='px-3 py-1.5 text-base font-normal mt-4 text-gray-700 bg-purple-50 bg-clip-padding rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:outline-none border border-purple-600'
+				/>
+				<input
+					value={paycheck}
+					onChange={(e) => setPaycheck(e.target.value)}
+					type='number'
+					placeholder='Wypłata'
+					className='px-3 py-1.5 text-base font-normal mt-4 text-gray-700 bg-purple-50 bg-clip-padding rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:outline-none border border-purple-600'
+				/>
+				<button
+					onClick={Submit}
+					className='px-3 py-1.5 text-base font-normal mt-4 text-white bg-purple-600 bg-clip-padding rounded transition ease-in-out hover:bg-purple-500 border border-purple-550'>
+					Dodaj
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default AddUser
