8948746e353cbc02826754fe77780dfcaa46589c
bar-85 <bajwolskabarbara@gmail.com>
2022-11-13 18:49:26 +0100

    add Link

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
 		<div className='w-full h-16 bg-purple-600 flex items-center px-10 py-2 justify-between'>
 			<h1 className='text-white text-3xl font-semibold font-Montserrat'>Lista płac</h1>
-			<button className='bg-transparent hover:bg-purple-500 font-semibold text-white py-2 px-4 border hover:border-white rounded'>Dodaj</button>
+			<Link to='add-user' className='bg-transparent hover:bg-purple-500 font-semibold text-white py-2 px-4 border hover:border-white rounded'>Dodaj</Link>
 		</div>
 	)
 }
